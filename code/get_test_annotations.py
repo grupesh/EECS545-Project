1d8c2e2e3578c208b960ec990ee5689c14810643
Rupesh Gupta <grupesh@umich.edu>
2020-10-26 15:58:53 -0400

    small update

--- code/get_test_annotations.py
+++ code/get_test_annotations.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+import cv2
 
 def get_test_annotations(f_path):
     """
@@ -23,7 +25,8 @@ def get_test_annotations(f_path):
         # print(file_name)
         category_name = line.split(' ')[2]
         # print(category_name)
-        video_path = '/content/drive/Shared drives/EECS 545 - ML Project/data/Video_files/'
+	# video_path is location of video files in database
+        video_path = 'G:/Shared drives/EECS 545 - ML Project/data/Video_files/'
         if category_name in ["Arson", "Arrest", "Abuse", "Assault"]:
             folder_name = 'Anomaly-Videos-Part-1/' + category_name + '/'
         elif category_name in ["Explosion", "Fighting", "Burglary"]:
@@ -84,9 +87,54 @@ def get_test_annotations(f_path):
             raise Exception('anomaly frame number cannot be 32')
 
         new['anomaly_frames'] = anomaly_frames
-        print('Test file : ', file_name, 'category : ', category_name, 'anomaly frames = ', anomaly_frames)
+        #print('Test file : ', file_name, 'category : ', category_name, 'anomaly frames = ', anomaly_frames)
         test_dset_list.append(new)
     return test_dset_list
+
+
+def count_frames(path, override=False):
+    # grab a pointer to the video file and initialize the total
+    # number of frames read
+    video = cv2.VideoCapture(path)
+    total = 0
+    # if the override flag is passed in, revert to the manual
+    # method of counting frames
+    if override:
+        total = count_frames_manual(video)
+    # otherwise, let's try the fast way first
+    else:
+        # lets try to determine the number of frames in a video
+        # via video properties; this method can be very buggy
+        # and might throw an error based on your OpenCV version
+        # or may fail entirely based on your which video codecs
+        # you have installed
+        try:
+            total = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        # uh-oh, we got an error -- revert to counting manually
+        except:
+            total = count_frames_manual(video)
+    # release the video file pointer
+    video.release()
+    # return the total number of frames in the video
+    return total
+
+
+def count_frames_manual(video):
+    # initialize the total number of frames read
+    total = 0
+    # loop over the frames of the video
+    while True:
+        # grab the current frame
+        (grabbed, frame) = video.read()
+
+        # check to see if we have reached the end of the
+        # video
+        if not grabbed:
+            break
+        # increment the total number of frames read
+        total += 1
+    # return the total number of frames in the video file
+    return total
 
 if __name__ == '__main__':
     f_path = os.path.join(os.getcwd(),"..",'Anomaly_Detection_splits','Temporal_Anomaly_Annotation_for_Testing_Videos.txt')
@@ -94,5 +142,5 @@ if __name__ == '__main__':
     print('Sample entry in the list : ', test_annotations[16])
     # save test_annotations to saved_data folder
     write_path = os.path.join(os.getcwd(),"..",'saved_data','test_annotations.pt')
-    if ~os.exists(write_path):
+    if not os.exists(write_path):
         torch.save(test_annotations,write_path)
